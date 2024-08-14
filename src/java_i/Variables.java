abccfb856c9567a941621a4e1d46f752201c9777
MEisSCAMMER <ishaan.ramesh@gmail.com>
2024-08-13 20:36:22 -0400

    init commit

---
+++ src/java_i/Variables.java
@@ -0,0 +1,12 @@
+package java_i;
+
+public class Variables {
+    public static void main(String[] args) {
+        // After watching the video, declare a variable of each type and assign it a value.
+        // Then, print all of them.
+
+        ///////////////////////////
+
+        ///////////////////////////
+    }
+}
