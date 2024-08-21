cae8da3e975d0fec42efe113120eaa805e1467c9
James Mou <95831855+Hammyboi@users.noreply.github.com>
2024-08-20 20:53:39 -0400

    Remove tracked class files

--- src/java_i/Variables.java
+++ src/java_i/Variables.java
@@ -2,7 +2,7 @@ package java_i;
 
 public class Variables {
     public static void main(String[] args) {
-        // After watching the video, declare a variable of each type and assign it a value.
+        // After watching the video, declare a variable of each type (boolean, int, double, String) and assign it a value.
         // Then, print all of them.
 
         ///////////////////////////
