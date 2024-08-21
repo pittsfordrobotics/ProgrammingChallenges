d1394ddce841e4e956423f5abc06056e6fd9ff2b
James Mou <95831855+Hammyboi@users.noreply.github.com>
2024-08-20 21:22:12 -0400

    Added more comments and instructions for Variables

--- src/java_i/Variables.java
+++ src/java_i/Variables.java
@@ -5,6 +5,26 @@ public class Variables {
         // After watching the video, declare a variable of each type (boolean, int, double, String) and assign it a value.
         // Then, print all of them.
 
+        // int a = 1;
+        // double b = 2;
+        // String c = "hi";
+
+        ///////////////////////////
+
+        ///////////////////////////
+
+        // Create another 2 ints and 2 doubles.
+
+        // int d = 3;
+        // int e = 4;
+        // double f = 5;
+        // double g = 6;
+        // String
+        // Add the ints and print the result
+        // System.out.println()
+
+        // Multiply an int and a double and print the result
+
         ///////////////////////////
 
         ///////////////////////////
