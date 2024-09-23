0e8a1616764008c41e09d6b2d58b2fd84b6f7ca2
MEisSCAMMER <ishaan.ramesh@gmail.com>
2024-09-22 21:36:27 -0400

    merging variables file

--- src/java_i/Variables.java
+++ src/java_i/Variables.java
@@ -6,14 +6,27 @@ public class Variables {
         So you've watched the video? Cool!
         Besides String, there are some other types of variables. These are not capitalized.
         - int is a type that stores whole numbers (integers).
+             int x = 2;
+        
         - double is a type that stores decimals.
-        - boolean is a type that stores a value of either true or false (no quotes). They might SEEM useless, and in
-          fact they are. They are completely useless and no this is not foreshadowing in any sense of the word.
+             double x = 2.5;
+
+        - boolean is a type that stores a value of either true or false (no quotes). These get helpful later.
+             boolean x = true;
 
         Now, declare a variable of each type (boolean, int, double, and String)
         and assign each a value.
         Then, print all of them.
         */
+
+        ///////////////////////////
+
+        ///////////////////////////
+
+        // Create another 2 ints and 2 doubles.
+        // Add the ints and print the result.
+        // Multiply an int and a double and print the result.
+        // Divide a double by the other double and print the result.
 
         ///////////////////////////
 
