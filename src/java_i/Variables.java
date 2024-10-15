428f255c2b677d4d1aad4dbdc8eda48065d953d3
PeihengOnGit <peiheng.liu.wang@gmail.com>
2024-10-15 19:56:51 -0400

    Wrote code according to instructions

--- src/java_i/Variables.java
+++ src/java_i/Variables.java
@@ -20,7 +20,14 @@ public class Variables {
         */
 
         ///////////////////////////
-
+        boolean a = false;
+        int b = 13;
+        double c = 1.1;
+        String phrase = "Hello there";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(phrase);
         ///////////////////////////
 
         // Create another 2 ints and 2 doubles.
@@ -29,7 +36,13 @@ public class Variables {
         // Divide a double by the other double and print the result.
 
         ///////////////////////////
-
+        int i1 = 36;
+        int i2 = 108;
+        double d1 = 1.5;
+        double d2 = 2.5;
+        System.out.println(i1+i2);
+        System.out.println(i1*d1);
+        System.out.println(d2/d1);
         ///////////////////////////
     }
 }
